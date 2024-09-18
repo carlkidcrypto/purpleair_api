433d18344d6a57e1d3726700db458f2593904091
Carlos <dose.lucky.sake@cloak.id>
2024-09-18 14:03:41 -0700

    request_sensor_historic_data
    - added privacy to request_sensor_historic_data

--- purpleair_api/PurpleAirReadAPI.py
+++ purpleair_api/PurpleAirReadAPI.py
@@ -171,6 +171,7 @@ class PurpleAirReadAPI:
         sensor_index,
         fields,
         read_key=None,
+        privacy=None,
         start_timestamp=None,
         end_timestamp=None,
         average=None,
@@ -182,22 +183,26 @@ class PurpleAirReadAPI:
 
         :param (optional) str read_key: This read_key is required for private devices. It is separate to the api_key and each sensor has its own read_key. Submit multiple keys by separating them with a comma (,) character for example: key-one,key-two,key-three.
 
-        :param (optional) int start_timestamp: The time stamp of the first required history entry. Query is executed using data_timestamp >= start_timestamp.
-                                               Time can be specified as a UNIX time stamp in seconds or an ISO 8601 string. https://en.wikipedia.org/wiki/ISO_8601.
-                                               The time_stamp column in the resulting JSON or CSV will be in the same format and or time zone that you use for this start_timestamp parameter.
-                                               If not specified, the last maximum time span for the requested average will be returned.
+        :param (optional) str privacy: The privacy of returned data. Sensors can record data while registered as public or private. Obtaining private data requires the sensor's read_key be provided.
+                                       `auto` (default if not specified) returns data with privacy matching the sensor's current registration.
+                                       `both` returns all data regardless of privacy and adds the private field to the returned data columns.
+                                       Allowed values: auto, public, private, both
 
-        :param (optional) int end_timestamp: The end time stamp of the history to return. Query is executed using data_timestamp < end_timestamp.
-                                             Time can be specified as a UNIX time stamp in seconds or an ISO 8601 string. https://en.wikipedia.org/wiki/ISO_8601.
-                                             If not specified, the maximum time span will be returned starting from the provided start_timestamp.
+        :param (optional) int start_timestamp: The time stamp of the first required history entry. Query is executed using data_time_stamp >= start_timestamp. This can be specified as a UNIX time stamp in seconds or an ISO 8601 string.
+                                               If not specified, the maximum amount of data for the requested average will be returned up to the provided end_timestamp.
+                                               The time_stamp column of data in the response's JSON or CSV will use the same format and/or time zone used in the start_timestamp.
 
-        :param (optional) int average: The desired average in minutes, one of the following: 0 (real-time), 10 (default if not specified), 30, 60, 360 (6 hour), 1440 (1 day)
-                                       Coming soon: 10080 (1 week), 44640 (1 month), 525600 (1 year).
+        :param (optional) int end_timestamp: The end time stamp of the history to return. Query is executed using data_time_stamp < end_timestamp. This can be specified as a UNIX time stamp in seconds or an ISO 8601 string.
+                                             If not specified, the maximum amount of data for the requested average will be returned starting from the provided start_timestamp.
+
+        :param (optional) int average: The desired average in minutes. One of the following:
+                                       0 (real-time), 10 (default if not specified), 30, 60, 360 (6 hour), 1440 (1 day), 10080 (1 week), 43200 (1 month), 525600 (1 year).
+                                       The amount of data that can be returned in a single response depends on the average used. Time limits for each average are found in our looping API calls community article.
 
         :param str fields: The 'Fields' parameter specifies which 'sensor data fields' to include in the response. Not all fields are available as history fields and we will be working to add more as time goes on. Fields marked with an asterisk (*) may not be available when using averages. It is a comma separated list with one or more of the following:
 
                            Station information and status fields:
-                           hardware*, latitude*, longitude*, altitude*, firmware_version*, rssi, uptime, pa_latency, memory,
+                           hardware*, latitude*, longitude*, altitude*, firmware_version*, private, rssi, uptime, pa_latency, memory
 
                            Environmental fields:
                            humidity, humidity_a, humidity_b, temperature, temperature_a, temperature_b, pressure, pressure_a, pressure_b
@@ -235,6 +240,7 @@ class PurpleAirReadAPI:
         # passed in. Turn them into a dict of optional parameters
         optional_parameters_dict = {
             "read_key": read_key,
+            "privacy": privacy,
             "start_timestamp": start_timestamp,
             "end_timestamp": end_timestamp,
             "modified_since": end_timestamp,
