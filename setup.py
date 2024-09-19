e334b3966d154f0c6a77008781631d6dc6910c40
Carlos <dose.lucky.sake@cloak.id>
2024-09-19 13:44:12 -0700

    V1.2.0
    - prep work for new alpha release

--- setup.py
+++ setup.py
@@ -1,34 +1,4 @@
 #!/usr/bin/env python3
 from setuptools import setup
-import os
 
-
-def read_file(filename):
-    with open(
-        os.path.join(os.path.dirname(__file__), filename), encoding="utf-8"
-    ) as file:
-        return file.read()
-
-
-setup(
-    name="purpleair_api",
-    version="1.1.4",
-    license="MIT",
-    author="Carlos Santos",
-    author_email="dose.lucky.sake@cloak.id",
-    long_description=read_file("README.md"),
-    long_description_content_type="text/markdown",
-    packages=["purpleair_api"],
-    url="https://github.com/carlkidcrypto/purpleair_api",
-    keywords=[
-        "purpleair_api",
-        "purple air api",
-        "purple_air",
-        "purple air",
-        "purpleair_sensor",
-        "purpleair sensor",
-        "purpleair-sensor",
-    ],
-    install_requires=["requests"],
-    platforms=["Windows 32/64", "Linux 32/64", "MacOS 32/64"],
-)
+setup()
