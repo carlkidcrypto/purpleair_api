a5d380dd874806839a8ebd1a6b60f28e0fd39603
Carlos <27721404+carlkidcrypto@users.noreply.github.com>
2025-10-27 21:36:16 -0700

    V1.3.1
    bump for new version
    run sphinx

--- sphinx_docs_build/source/conf.py
+++ sphinx_docs_build/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2024, carlkidcrypto"
 author = "carlkidcrypto"
 
 # The full version, including alpha/beta/rc tags
-release = "V1.3.0"
+release = "V1.3.1"
 
 
 # -- General configuration ---------------------------------------------------
