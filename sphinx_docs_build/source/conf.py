08a6c7f5e77a66099e2a370d18e9acbaec37079a
Carlos <27721404+carlkidcrypto@users.noreply.github.com>
2025-11-23 19:40:31 -0800

    Add versioned Sphinx docs and update theme
    Renamed existing Sphinx HTML documentation to docs/html_v1.3.1 for versioning and updated docs/index.html to provide a landing page with links to multiple documentation versions. Switched Sphinx theme from sphinx_rtd_theme to furo in requirements.txt and conf.py, and added breathe_projects config for Doxygen integration.

--- sphinx_docs_build/source/conf.py
+++ sphinx_docs_build/source/conf.py
@@ -31,7 +31,11 @@ release = "V1.4.0.a1"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc"]
+extensions = [
+    "sphinx.ext.autodoc",
+]
+
+breathe_projects = {"EzSnmp": "../../doxygen_docs_build/doxygen/xml"}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -47,7 +51,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
